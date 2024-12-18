634ffe7e8ce0cec41133cffb728fcbe0915024af
Usman Ahmad <60046773+usmanh333@users.noreply.github.com>
2024-12-18 13:13:42 +0500

    Update car.js

--- routes/car.js
+++ routes/car.js
@@ -18,7 +18,7 @@ const router = express.Router();
 // router.post('/submit', authMiddleware, upload.array('images', 10), async (req, res) => {
 router.post('/submit', authMiddleware, async (req, res) => {
   const { carModel, price, phone, city } = req.body;
-  const imageUrls = req.files.map((file) => `/uploads/${file.filename}`);
+  // const imageUrls = req.files.map((file) => `/uploads/${file.filename}`);
 
   try {
     const car = new Car({
